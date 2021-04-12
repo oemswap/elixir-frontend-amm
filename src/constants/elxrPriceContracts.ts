09bc8971b1becfeb6bc1f4556eb9afdfdef98236
deazyyy <d.s@vimerco.com>
2021-04-13 02:01:22 +0530

    first

---
+++ src/constants/elxrPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {elxrAddress: string, busdAddress: string, lpAddress:string} = {
+  elxrAddress: '0xD6617f34E8Bd7a88E55Aa01C2608c81b4afFD0F6',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x83c854B8bE0a3C5985ce3C40Cf04A1fB825F4325'
+}
+
+export default priceContracts
